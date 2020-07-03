5ac7c87c07d6f43e271c099a036c08c9a1b725d2
Lucas Carvalho Honorio <caldoxgamer@gmail.com>
2020-07-03 11:56:19 -0300

    Updates

---
+++ precache-manifest.849ac10d7b6b8548ad05be39ff96e235.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941e576196eaf36607ff0441edce1be4",
+    "url": "/discord-template/index.html"
+  },
+  {
+    "revision": "525e5d8c8e3479d5d5a0",
+    "url": "/discord-template/static/css/2.8b6ab871.chunk.css"
+  },
+  {
+    "revision": "525e5d8c8e3479d5d5a0",
+    "url": "/discord-template/static/js/2.302f75eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discord-template/static/js/2.302f75eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8606241410025690374",
+    "url": "/discord-template/static/js/main.1df01735.chunk.js"
+  },
+  {
+    "revision": "2c599b7235df99dc5508",
+    "url": "/discord-template/static/js/runtime-main.5a298408.js"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/discord-template/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/discord-template/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/discord-template/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/discord-template/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/discord-template/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/discord-template/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/discord-template/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/discord-template/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/discord-template/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/discord-template/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/discord-template/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/discord-template/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/discord-template/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/discord-template/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/discord-template/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "97207deb3819adf4183a5caff14a14a2",
+    "url": "/discord-template/static/media/hacker-icon.97207deb.png"
+  }
+]);
